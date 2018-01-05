769593666eec19eaeb3cfa7ccf1c4b004808a0ef
Teceler <19716939+teceler@users.noreply.github.com>
2018-01-04 20:41:24 -0600

    stash

--- spec/features/css_spec.rb
+++ spec/features/css_spec.rb
@@ -1,19 +1,128 @@
 require "spec_helper"
 
-RSpec.feature "Recently updated renders the same", :type => :feature, :js => true do
+RSpec.feature "Renders the same:", :type => :feature, :js => true do
   before(:each) do
     4.times { create(:board).destroy }
-    desired_time = Time.new(2018)
+    desired_time = Time.zone.local(2018)
     Timecop.freeze(desired_time) do
       26.times { create(:post) }
     end
   end
 
-  scenario "in default" do
-    user = login
-    user.update_attributes(layout: 'default')
-    visit posts_path
-    puts page.text
-    expect(page).to match_expectation
+  scenario "default"
+    before(:each) do
+      user = login
+      user.update_attributes(layout: nil)
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+
+    scenario "User#Edit" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit edit_user_path(user.id)
+      end
+      expect(page).to match_expectation
+    end
   end
-end
+
+  scenario "dark"
+    before(:each) do
+      user = login
+      user.update_attributes(layout: 'dark')
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+  end
+
+  scenario "starry" do
+    before(:each) do
+      user = login
+      user.update_attributes(layout: 'starry')
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+  end
+
+  scenario "starrydark" do
+    before(:each) do
+      user = login
+      user.update_attributes(layout: 'starrydark')
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+
+  scenario "starrylight" do
+    before(:each) do
+      user = login
+      user.update_attributes(layout: 'starrylight')
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+  end
+
+  scenario "monochrome" do
+    before(:each) do
+      user = login
+      user.update_attributes(layout: 'monochrome')
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+  end
+
+  scenario "river" do
+    before(:each) do
+      user = login
+      user.update_attributes(layout: 'river')
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+  end
+
+  scenario "iconless" do
+    before(:each) do
+      user = login
+      user.update_attributes(layout: 'iconless')
+    end
+
+    scenario "Recently Updated" do
+      Timecop.freeze(Time.zone.local(2018)) do
+        visit posts_path
+      end
+      expect(page).to match_expectation
+    end
+  end

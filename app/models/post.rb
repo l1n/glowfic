b4d59e2dc4504755ea26fd97935d50fab758c758
Teceler <19716939+teceler@users.noreply.github.com>
2018-02-11 12:04:05 -0600

    Add `editable_by?` and `deletable_by?` to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -275,6 +275,18 @@ class Post < ApplicationRecord
     self.replies.ordered.first
   end
 
+  def editable_by?(editor)
+    return false unless editor
+    return true if editor.id == user_id
+    editor.has_permission?(:edit_replies)
+  end
+
+  def deletable_by?(editor)
+    return false unless editor
+    return true if editor.id == user_id
+    editor.has_permission?(:delete_replies)
+  end
+
   private
 
   def valid_board

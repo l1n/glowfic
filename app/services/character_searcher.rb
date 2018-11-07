2d6ae501e177663845a11c67480c4ce7dac8774a
teceler <19716939+teceler@users.noreply.github.com>
2018-11-06 21:47:50 -0600

    Split out CharacterSearcher

--- app/services/character_searcher.rb
+++ app/services/character_searcher.rb
@@ -1,14 +1,26 @@
 class CharacterSearcher < Searcher
-  def search(user_id: nil, template_id: nil, name: nil, search_name: false, search_screenname: false, search_nickname: false)
-    if user_id.present?
-      @users = User.where(id: user_id)
-      if @users.present?
-        @search_results = @search_results.where(user_id: user_id)
-      else
-        flash.now[:error] = "The specified author could not be found."
-      end
-    end
+  def search(user_id: nil, template_id: nil, name: nil, search_name: nil, search_screenname: nil, search_nickname: nil)
+    @search_results = search_users(user_id) if user_id.present?
 
+    @search_results = search_templates(template_id, user_id) if template_id.present || user_id.present
+
+    @search_results = search_names(name: name, search_name: search_name, search_screenname: search_screenname, search_nickname: search_nickname) if name.present?
+
+    @search_results = do_search(@search_results)
+  end
+
+  private
+
+  def search_users(user_id)
+    @users = User.where(id: user_id)
+    if @users.present?
+      @search_results = @search_results.where(user_id: user_id)
+    else
+      flash.now[:error] = "The specified author could not be found."
+    end
+  end
+
+  def search_templates(template_id, user_id)
     if template_id.present?
       @templates = Template.where(id: template_id)
       template = @templates.first
@@ -25,16 +37,14 @@ class CharacterSearcher < Searcher
     elsif user_id.present?
       @templates = Template.where(user_id: user_id).ordered.limit(25)
     end
+  end
 
-    if name.present?
-      where_calc = []
-      where_calc << "name LIKE ?" if search_name.present?
-      where_calc << "screenname LIKE ?" if search_screenname.present?
-      where_calc << "template_name LIKE ?" if search_nickname.present?
+  def search_names(name:, search_name: nil, search_screenname: nil, search_nickname: nil)
+    where_calc = []
+    where_calc << "name LIKE ?" if search_name.present?
+    where_calc << "screenname LIKE ?" if search_screenname.present?
+    where_calc << "template_name LIKE ?" if search_nickname.present?
 
-      @search_results = @search_results.where(where_calc.join(' OR '), *(['%' + name.to_s + '%'] * where_calc.length))
-    end
-
-    @search_results = @search_results.ordered.paginate(page: page, per_page: 25)
+    @search_results.where(where_calc.join(' OR '), *(['%' + name.to_s + '%'] * where_calc.length))
   end
 end

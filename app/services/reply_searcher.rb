a8b256c2ff20b6d175b64bda708e6b6d565c4e3b
teceler <19716939+teceler@users.noreply.github.com>
2018-11-06 21:18:21 -0600

    Replace params on ReplySearcher

--- app/services/reply_searcher.rb
+++ app/services/reply_searcher.rb
@@ -1,12 +1,12 @@
 class ReplySearcher < Searcher
   def search(user_id: nil, character_id: nil, icon_id: nil, subj_content: nil, sort: nil, post: nil, template_id: nil, condensed: nil)
-    @search_results = @search_results.where(user_id: params[:author_id]) if params[:author_id].present?
-    @search_results = @search_results.where(character_id: params[:character_id]) if params[:character_id].present?
-    @search_results = @search_results.where(icon_id: params[:icon_id]) if params[:icon_id].present?
+    @search_results = @search_results.where(user_id: user_id) if user_id.present?
+    @search_results = @search_results.where(character_id: character_id) if character_id.present?
+    @search_results = @search_results.where(icon_id: icon_id) if icon_id.present?
 
-    if params[:subj_content].present?
-      @search_results = @search_results.search(params[:subj_content]).with_pg_search_highlight
-      exact_phrases = params[:subj_content].scan(/"([^"]*)"/)
+    if subj_content.present?
+      @search_results = @search_results.search(subj_content).with_pg_search_highlight
+      exact_phrases = subj_content.scan(/"([^"]*)"/)
       if exact_phrases.present?
         exact_phrases.each do |phrase|
           phrase = phrase.first.strip
@@ -16,30 +16,30 @@ class ReplySearcher < Searcher
       end
     end
 
-    append_rank = params[:subj_content].present? ? ', rank DESC' : ''
-    if params[:sort] == 'created_new'
+    append_rank = subj_content.present? ? ', rank DESC' : ''
+    if sort == 'created_new'
       @search_results = @search_results.except(:order).order('replies.created_at DESC' + append_rank)
-    elsif params[:sort] == 'created_old'
+    elsif sort == 'created_old'
       @search_results = @search_results.except(:order).order('replies.created_at ASC' + append_rank)
-    elsif params[:subj_content].blank?
+    elsif subj_content.blank?
       @search_results = @search_results.order('replies.created_at DESC')
     end
 
-    if @post
-      @search_results = @search_results.where(post_id: @post.id)
+    if post
+      @search_results = @search_results.where(post_id: post.id)
     elsif params[:board_id].present?
       post_ids = Post.where(board_id: params[:board_id]).pluck(:id)
       @search_results = @search_results.where(post_id: post_ids)
     end
 
-    if params[:template_id].present?
-      @templates = Template.where(id: params[:template_id])
+    if template_id.present?
+      @templates = Template.where(id: template_id)
       if @templates.first.present?
         character_ids = Character.where(template_id: @templates.first.id).pluck(:id)
         @search_results = @search_results.where(character_id: character_ids)
       end
-    elsif params[:author_id].present?
-      @templates = @templates.where(user_id: params[:author_id])
+    elsif user_id.present?
+      @templates = @templates.where(user_id: user_id)
     end
 
     @search_results = @search_results
@@ -51,7 +51,7 @@ class ReplySearcher < Searcher
       .paginate(page: page, per_page: 25)
       .includes(:post)
 
-    unless params[:condensed]
+    unless condensed
       @search_results = @search_results
         .select('icons.keyword, icons.url')
         .left_outer_joins(:icon)

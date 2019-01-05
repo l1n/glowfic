e44e5ff6797791d77a9ac6b086e4476436907af5
teceler <19716939+teceler@users.noreply.github.com>
2019-01-05 17:20:29 -0600

    stash

--- app/services/post_list.rb
+++ app/services/post_list.rb
@@ -5,34 +5,33 @@ class PostList
   attr_reader :errors
 
   def initialize(relation, no_tests: true, with_pagination: true, select: '')
-    @posts = expand(relation)
     @no_tests = no_tests
     @pagination = with_pagination
     @select = select
     @errors = ActiveModel::Errors.new(self)
+    @posts = expand_posts(relation)
+    format_posts
   end
 
-  def posts
-    posts = posts.paginate(page: page, per_page: 25) if with_pagination
-    posts = posts.no_tests if no_tests
+  def format_posts
+    @posts = @posts.paginate(page: page, per_page: 25) if with_pagination
+    @posts = @posts.no_tests if no_tests
 
     if logged_in?
       @opened_ids ||= PostView.where(user_id: current_user.id).where('read_at IS NOT NULL').pluck(:post_id)
 
-      opened_posts = PostView.where(user_id: current_user.id).where('read_at IS NOT NULL').where(post_id: posts.map(&:id)).select([:post_id, :read_at])
+      opened_posts = PostView.where(user_id: current_user.id).where('read_at IS NOT NULL').where(post_id: @posts.map(&:id)).select([:post_id, :read_at])
       @unread_ids ||= []
       @unread_ids += opened_posts.select do |view|
-        post = posts.detect { |p| p.id == view.post_id }
+        post = @posts.detect { |p| p.id == view.post_id }
         post && view.read_at < post.tagged_at
       end.map(&:post_id)
     end
-
-    posts
   end
 
   private
 
-  def expand(relation)
+  def expand_posts(relation)
     posts = relation
       .select('posts.*, boards.name as board_name, users.username as last_user_name'+ select)
       .joins(:board)
